2b82749cc1d337f27957dc16c0c197a7bcc179c2
Richard Xiong <richardx366@gmail.com>
2022-03-21 00:21:03 -0400

    stepper finish

--- projects/stepper.ts
+++ projects/stepper.ts
@@ -9,9 +9,13 @@ export default async function stepper() {
   const parser = arduino.pipe(new ReadlineParser({ delimiter: '\r\n' }));
   parser.on('data', (unparsedData: Buffer) => {
     const data = unparsedData.toString();
-    if (data === 'stepped') socket.emit('stepped');
+    if (data === 'done') socket.emit('done');
+    console.log(data);
   });
   socket.on('step', (steps) => {
-    arduino.write(steps);
+    arduino.write(`step:${steps}`);
+  });
+  socket.on('pins', (pins) => {
+    arduino.write(`pins:${pins}`);
   });
 }
